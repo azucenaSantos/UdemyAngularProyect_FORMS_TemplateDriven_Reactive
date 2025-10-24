cb4f548956733f75f16e0ff68076a916053259f1
Azucena <azu.santos.ete@gmail.com>
2025-10-24 10:19:58 +0200

    Reactive Forms Final (checkboxes, formGroups anidados, validacion personalizada a varios controladores dentro de un formGroup)

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  AbstractControl,
+  FormArray,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+
+// function equalValues(control: AbstractControl) {
+//   const password = control.get('password')?.value; //recogemos el control con este valor asociado
+//   const confirmPassword = control.get('confirmPassword')?.value;
+
+//   if (password === confirmPassword) {
+//     return null;
+//   }
+
+//   return { passwordsNotEquals: true };
+// }
+//La funcion la podemos hacer mas reutilizable
+
+function equalValues(controlName1: string, controlName2: string) {
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value;
+    const val2 = control.get(controlName2)?.value;
+    if (val1 === val2) {
+      return null;
+    }
+    return { valuesNotEquals: true };
+  };
+}
 
 @Component({
   selector: 'app-signup',
@@ -18,30 +43,50 @@ export class SignupComponent implements OnInit {
     email: new FormControl('', {
       validators: [Validators.required, Validators.email],
     }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)],
-    }),
-    confirmPassword: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)],
-    }),
+    //Vamos a unificar la contraseña y la verificacion en un único control
+    //-> visualizaremos el contenido de todo el form de una forma más legible y unificada
+    //ya que los formGroup anidados crearán un objeto con los controladores dentro de ellos
+    passwords: new FormGroup(
+      {
+        //será un formGroup dentro de un formGroup principal
+        password: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)],
+        }),
+        confirmPassword: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)],
+        }),
+        //de esta forma se estructura mejor los contenidos de un formulario
+      },
+      //El formGroup tiene un segundo parámetro que será la validacion conjunta de todos sus controladores
+      //en este caso una validacion de que el valor de password y de confirmPassword sea igual :D
+      {
+        validators: [equalValues('password', 'confirmPassword')], //añadiomos la validacion conjunta que hemos creado
+        //Cuando no son valores iguales angular asocia a los controles una clase de ng-invalid
+        //aunque no mostremos un error podemos incluir estilos css para esa clase y hacer que el usuario
+        //se entere que no son valores iguales
+      }
+    ),
     firstName: new FormControl('', {
       validators: [Validators.required],
     }),
     lastName: new FormControl('', {
       validators: [Validators.required],
     }),
-    street: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    number: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    postalCode: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    city: new FormControl('', {
-      validators: [Validators.required],
-    }),
+    //Creamos tambien un formGroup anidado para los datos de direccion
+    address: new FormGroup({
+      street: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      number: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      postalCode: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      city: new FormControl('', {
+        validators: [Validators.required],
+      }),
+    }), //ahora los controles están anidados dentro del address
     //El role tendrá un valor por defecto de los que ya tiene el select puesto en la template
     //Además le pasamos los unicos valores que podrá tener el control en los <...>
     role: new FormControl<
@@ -49,6 +94,14 @@ export class SignupComponent implements OnInit {
     >('student', {
       validators: [Validators.required],
     }),
+    //¿Cómo gestionamos los checkbox de una forma óptima? --> FormArray
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+      //Estos 3 formControl representan los 3 checkboxs
+      //Cuando se marque una de las opciones, se guardará true en la posicion del array source (false, true, true -> por ejemplo)
+    ]),
     //El valor por defecto del ultimo check es false (no marcado)
     agree: new FormControl(false, {
       validators: [Validators.required],
@@ -59,6 +112,11 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {}
 
   onSubmit() {
+    if (this.form.invalid) {
+      console.log('Invalid form');
+      return;
+    }
+
     console.log(this.form);
   }
   onReset() {
